6321e7dba23daefe6b129b54a4e46b98c4eea829
Canhui Huang <canhui.huang@mobicash.mx>
2021-08-29 22:16:33 -0600

    assingments

---
+++ session 1/assignment.js
@@ -0,0 +1,24 @@
+// Change <script src="app.js"></script> to <script src="assignments.js"></script>
+// Then solve 1, 2, & 3.
+
+/* Example: Change the value of x to 5. Then print x in the console. */
+
+var x = 2;
+// Code here:
+x = 5;
+console.log(x);
+
+/* 1: Change the value of person to "Ana". Then print person in the console. */
+
+let person = 'John';
+// Code here:
+
+/* 2: Create three variables called a, b, & c with the keyword const. a must be a string, b must be a number, c must be a boolean. Then print a, b, & c in the console. */
+
+// Code here:
+
+/* 3: Print the number version of foo, & print the string version of boo. */
+
+var foo = '900';
+var boo = 37;
+// Code here:
