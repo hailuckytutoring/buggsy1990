40c30f8d5a70e207043dcd899d8c838da7223858
Canhui Huang <canhui.huang@mobicash.mx>
2021-09-01 22:50:09 -0600

    sessioon 2 with exercises

---
+++ session 2/app.js
@@ -0,0 +1,26 @@
+// functions
+
+// Define / Create a function
+function add(a, b) {
+  console.log(a + b);
+  console.log(a + b);
+  console.log(a + b);
+}
+
+function sayHello() {
+  console.log('Hello' + ' World');
+}
+
+// return me the result of the addition of two numbers
+function getResultofAddition(a, b) {
+  return a + b;
+}
+
+// Calling add
+add(5, 7);
+
+add('Hello', ' World');
+
+// Calling getResultofAddition
+
+var x = getResultofAddition(6, 10);
---
+++ session 2/exercises.js
@@ -0,0 +1,47 @@
+// Change <script src="app.js"></script> to <script src="exercises.js"></script>
+// Then solve 1 & 2.
+
+/* Example: 
+1 - Define a function called sayHello that takes a parameter called 'name'.
+2 - Write the code in the body of sayHello that will print "Hello {name}" in the console.
+3 - call the sayHello function twice with the following expected output:
+    Hello Ana
+    Hello John
+*/
+
+// Type your code solution below:
+
+function sayHello(name) {
+  console.log('Hello' + name);
+}
+
+sayHello('Ana');
+sayHello('John');
+
+/* -------------------------------------- */
+
+/* 1: 
+1 - Define a function called printSquareOf that takes a parameter called 'x'.
+2 - Write the code in the body of printSquareOf that will print the square of x in the console.
+3 - call the printSquareOf function twice with the following expected output:
+    25
+    64
+*/
+
+// Type your code solution below:
+
+/* -------------------------------------- */
+
+/* 2: 
+1 - Create / Define two functions:
+    getSquareOf: takes one parameter, & it should return the square of the parameter.
+    sumOf: takes two parameters, & it should return the sum of the two parameters.
+2 - If everything done correctly, once you save the file, you should see "Result: 40" in the browser's console.
+*/
+
+// Type your code solution below:
+
+var result = getSquareOf(5) + sumOf(7, 8);
+console.log('Result: ', result);
+
+/* -------------------------------------- */
